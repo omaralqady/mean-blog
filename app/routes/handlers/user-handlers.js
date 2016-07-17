8b4dd5464ea993ac6602e4a34d80e10915a5e6d6
Omar Alqady <omaralqady@gmail.com>
2016-07-17 14:25:48 +0200

    Improve user/register validation error messages

--- app/routes/handlers/user-handlers.js
+++ app/routes/handlers/user-handlers.js
@@ -1,13 +1,13 @@
 /*** requirements ***/
 
 //external
-var logger = require('log4js').getLogger();
-var bcrypt = require('bcryptjs');
-var Joi = require('joi');
+var logger = require( 'log4js' ).getLogger();
+var bcrypt = require( 'bcryptjs' );
+var Joi = require( 'joi' );
 //
 
 //models
-var User = require('../../models/user.js');
+var User = require( '../../models/user.js' );
 //
 
 /*** end of requirements ***/
@@ -17,73 +17,98 @@ var User = require('../../models/user.js');
 var userHandlers = {
 
 	//post req on /user/register
-	register: function(req, res, next) {
+	register: function( req, res, next ) {
 
 		//saves object to db
-		function saveUser(userData) {
+		function saveUser( userData ) {
 
-			var newUser = new User(userData);
+			var newUser = new User( userData );
 
-			newUser.save(function(err, user) {
-				if(err) {
+			newUser.save( function( err, user ) {
+				if ( err ) {
 
-					logger.debug('Error inserting data into DB: ', err);
-					logger.trace('Erroneous data: ', userData);
+					logger.debug( 'Error inserting data into DB: ', err );
+					logger.trace( 'Erroneous data: ', userData );
 
-					if( err.code === 11000 ) {
+					if ( err.code === 11000 ) {
 
-						res.status(409).json({message: 'Duplicate username and/or email address.'});
+						res.status( 409 ).json( { message: 'Duplicate username and/or email address.' } );
 					} else {
-						
-						res.status(500).json({message: 'Error inserting user data into DB.'});
+
+						res.status( 500 ).json( { message: 'Error inserting user data into DB.' } );
 					}
 				} else {
-					res.redirect('/login');
+					res.redirect( '/login' );
 				}
-			});
+			} );
 		}
 
 		//creates user data object, and hashes password using bcrypt
-		function registerUser(userData) {
+		function registerUser( userData ) {
 
 			var plainPassword = userData.password;
 
-			bcrypt.hash(plainPassword, 8, function(err, hash) {
-				if(err) {
-					res.status(500).json({message: 'Error storing user data in DB.'});
+			bcrypt.hash( plainPassword, 8, function( err, hash ) {
+				if ( err ) {
+					res.status( 500 ).json( { message: 'Error storing user data in DB.' } );
 				} else {
 					userData.password = hash;
-					saveUser(userData);
+					saveUser( userData );
 				}
-			});
+			} );
 		}
 
-		var userData = Object.assign({}, req.body);
+		var userData = Object.assign( {}, req.body );
 
-		var userSchemaJoi = Joi.object().keys({
-			name: Joi.string().trim().regex(/^([a-z\s]){4,64}$/gi).required(),
-			username: Joi.string().trim().alphanum().min(4).max(32).required(),
-			password: Joi.string().min(4).max(32).required(),
-			confirmPassword: Joi.string().equal(Joi.ref('password')).required(),
+		var userSchemaJoi = Joi.object().keys( {
+			name: Joi.string().trim().regex( /^([a-z\s]){4,64}$/gi ).required(),
+			username: Joi.string().trim().alphanum().min( 4 ).max( 32 ).required(),
+			password: Joi.string().min( 4 ).max( 32 ).required(),
+			confirmPassword: Joi.string().equal( Joi.ref( 'password' ) ).required(),
 			email: Joi.string().trim().email().required(),
-			confirmEmail: Joi.string().trim().equal(Joi.ref('email')).email().required(),
+			confirmEmail: Joi.string().trim().equal( Joi.ref( 'email' ) ).email().required(),
 			dob: Joi.date().required(),
-		});
+		} );
 
-		Joi.validate(userData, userSchemaJoi, function(err, value) {
-			if(err) {
+		Joi.validate( userData, userSchemaJoi, function( err, value ) {
+			if ( err ) {
+				var errMessage;
+				logger.debug( 'Error validating user data. User data: ', value,
+					'\nError: ', err );
+				//res.status(409).json({message: 'Invalid user data sent.'});
+				if ( err.details[ 0 ].path === 'name' ) {
+					errMessage =
+						'The \'Name\' field has to contain letters only, and has to be between 4 and 64 characters long.';
+				} else if ( err.details[ 0 ].path === 'username' ) {
+					errMessage =
+						'The \'Username\' field can only contain letters and numbers, and has to be 4-32 characters long.';
+				} else if ( err.details[ 0 ].path === 'password' ) {
+					errMessage = 'The \'Password\' field has to be 4-32 characters long.';
+				} else if ( err.details[ 0 ].path === 'confirmPassword' ) {
+					errMessage =
+						'The \'Confirm Password\' field has to match the \'password\'!';
+				} else if ( err.details[ 0 ].path === 'email' ) {
+					errMessage = 'The \'Email\' field has to be a proper email address.';
+				} else if ( err.details[ 0 ].path === 'confirmEmail' ) {
+					errMessage =
+						'The \'Confirm Email\' field has to match the \'email\' field!';
+				} else if ( err.details[ 0 ].path === 'dob' ) {
+					errMessage =
+						'The \'Date of birth\' field is required and has to be in the format \'mm/dd/YYY\'.';
+				} else {
+					errMessage = 'Form data is invalid!';
+				}
 
-				logger.debug('Error validating user data. User data: ', value, '\nError: ', err);
-				res.status(409).json({message: 'Invalid user data sent.'});
+				res.status( 409 ).json( { message: errMessage } );
 			} else {
 
 				//remove unnecessary fields
-				delete value['confirmEmail'];
-				delete value['confirmPassword'];
+				delete value[ 'confirmEmail' ];
+				delete value[ 'confirmPassword' ];
 
-				registerUser(value);
+				registerUser( value );
 			}
-		});
+		} );
 	},
 };
 
--- public/app/controllers/user-register-controller.js
+++ public/app/controllers/user-register-controller.js
@@ -9,30 +9,27 @@ blog.controller( 'userRegistrationController', function( $scope, $http, $window 
 
 		$scope.validationError = false;
 
-		if ( userData.password !== undefined && userData.password.length >= 4 ) {
-			if ( userData.confirmPassword !== undefined && userData.password ===
-				userData.confirmPassword ) {
-				if ( userData.username !== undefined && userData.username.length >= 4 ) {
-					if ( userData.email !== undefined && userData.confirmEmail !== undefined &&
-						userData.email === userData.confirmEmail ) {
-						if ( userData.dob !== null ) {
-							return true;
-						} else {
-							errorMessage = 'You must enter your date of birth';
-						}
-					} else {
-						errorMessage = 'Email and Confirm Email do not match';
-					}
-				} else {
-					errorMessage = 'Username is less than 4 characters long';
-				}
-
-			} else {
-				errorMessage = 'Password and Confirm Password do not match';
-			}
-		} else {
+		if ( userData.name === undefined || userData.name.length < 4 ) {
+			errorMessage = 'Name is less than 4 characters long';
+		} else if ( userData.username === undefined || userData.username.length < 4 ) {
+			errorMessage = 'Username is less than 4 characters long';
+		} else if ( userData.password === undefined || userData.password.length < 4 ) {
 			errorMessage = 'Password is less than 4 characters long';
+		} else if ( userData.confirmPassword === undefined || userData.confirmPassword !==
+			userData.password ) {
+			errorMessage = 'Password and Confirm Password do not match';
+		} else if ( userData.email === undefined ) {
+			errorMessage = 'You must enter your email';
+		} else if ( userData.confirmEmail !== userData.email ) {
+			errorMessage = 'Email and Confirm Email do not match';
+		} else if ( userData.dob == undefined || userData.dob === null ) {
+			errorMessage = 'You must enter your date of birth';
+		} else {
+			return true;
 		}
+
+		//in case an error has been detected above, otherwise, 'true' is returned in the 'else' 
+		// clause to denote validity
 		$scope.validationError = true;
 		$scope.errorMessage = errorMessage;
 
@@ -54,26 +51,26 @@ blog.controller( 'userRegistrationController', function( $scope, $http, $window 
 		if ( isFormValid( userData ) ) {
 
 			$http.post( '/api/user/register', userData )
-			.then( function( res ) {
+				.then( function( res ) {
 
-				if ( res.status === 200 ) {
+					if ( res.status === 200 ) {
 
-					$window.location.href = '/login';
-				} else {
-					
-					alert( res.data );
-				}
-			} )
-			.catch( function( err ) {
+						$window.location.href = '/login';
+					} else {
 
-				if ( err.data.message ) {
+						alert( res.data );
+					}
+				} )
+				.catch( function( err ) {
 
-					alert( err.data.message );
-				} else {
+					if ( err.data.message ) {
 
-					alert( 'Error sending data to server. Please try again.' );
-				}
-			} );
+						alert( err.data.message );
+					} else {
+
+						alert( 'Error sending data to server. Please try again.' );
+					}
+				} );
 		}
 	};
 } );
